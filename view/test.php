f26ce3cb387b4aaa51268a45c3b6fa4c5f4575ef
shahidee44 <36878360+shahidee44@users.noreply.github.com>
2019-01-20 00:41:44 +0800

    Update test.php

--- view/test.php
+++ view/test.php
@@ -14,16 +14,67 @@
 </head>
 <body>
 	<?php create_ad() ?>
-	<form method="post" action="login.php" class="container text-center">
+	<form method="post" action="test.php" class="container text-center">
 		<fieldset>
-			<legend> Log In Information Here </legend>
-			<p>
-				<label>Username: <input type="text" name="name" size="20" maxlength="40"></label>
+			<legend> Claim from Faucet </legend>
+			<p> Claim 20 credits every 15 minutes </p>
+			<p> Credit Balance: 
+				<?php 
+
+				require("../mysqli_connect.php");
+
+				date_default_timezone_set('Asia/Singapore');
+				$name = 'admin';//SESSION NAME HERE
+				$time = date('Y-m-d H:i:s');
+
+				$q = "SELECT * FROM `user` WHERE `name` = '$name'";
+
+				$r = @mysqli_query($dbc, $q);
+
+				$row = @mysqli_fetch_assoc($r);
+
+				$time_now = $row['last_claim_date_time'];
+
+				$time_claim = date('Y-m-d H:i:s', strtotime('+15 minutes', strtotime($time_now)));
+
+				if ($_SERVER['REQUEST_METHOD'] == 'POST' && $time > $time_claim) {
+
+					$new_amount = $row['credit'] + 20;
+
+					mysqli_free_result($r);
+
+					$q = "UPDATE `user` SET `credit` = '$new_amount', `last_claim_date_time` = NOW() WHERE `name` = '$name' " ;
+
+					$r = @mysqli_query($dbc, $q);
+
+					$q = "SELECT * FROM `user` WHERE `name` = '$name'";
+
+					$r = @mysqli_query($dbc, $q);
+
+					$row = @mysqli_fetch_assoc($r);
+
+					echo('<strong>'.$row['credit'].'</strong>');
+
+				} else {
+
+					echo('<strong>'.$row['credit'].'</strong>');
+
+					if ($time < $time_claim) {
+
+						echo('<p>You have claimed within the past 15 minutes</p>');
+
+					}
+				
+				}
+
+				echo('<p> Last Claim Time: '.$time_now.'</p>
+					<p> Next Claim Valid Time: '.$time_claim.'</p>');
+
+				mysqli_close($dbc);
+
+				?>
 			</p>
-			<p>
-				<label>Password: <input type="password" name="password" size="20" maxlength="40"></label>
-			</p>
-			<input type="submit" name="submit" value="Log in">
+			<input type="submit" name="submit" value="Claim">
 		</fieldset>
 	</form>
 
