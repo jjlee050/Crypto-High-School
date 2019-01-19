bfa41caeb78688504f17f53cef1dba8e3c7fec1d
jjlee050 <josephlee050@gmail.com>
2019-01-19 16:07:46 +0800

    Edit menu

--- view/mainmenu.php
+++ view/mainmenu.php
@@ -1,10 +1,16 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <ul class="navbar-nav mr-auto">
+<nav class="navbar navbar-dark bg-dark">
+  <ul class="nav mr-auto">
     <li class="nav-item">
       <a class="nav-link" href='main' title='Home'>Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href='login/runLogout'>Logout</a>
+      <a class="nav-link" href='login'>Login</a>
     </li>
+    <li class="nav-item">
+      <a class="nav-link" href='signup' title='Signup'>Signup</a>
+    </li>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
   </ul>
 </div>
---
+++ view/signup.php
@@ -0,0 +1,22 @@
+<div id="signup">
+	<!-- Test -->
+	<form action="main/signup" method="post">
+		<div class="form-group">
+			<label for="name"> Name: </label>
+			<input type="text" class="form-control" name="name" />
+		</div>
+		<div class="form-group">
+			<label for="email"> Email: </label>
+			<input type="email" class="form-control" name="email" />
+		</div>
+		<div class="form-group">
+			<label for="password"> Password: </label>
+			<input type="password" class="form-control" name="password" />
+		</div>
+		<div class="form-group">
+			<label for="confirm_password"> Confirm Password: </label>
+			<input type="password" class="form-control" name="confirm_password" />
+		</div>
+		<input type="submit" value="Register" class="btn btn-primary"/>
+    </form>
+</div>
