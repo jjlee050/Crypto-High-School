b202ae5b43140e72ccc2b8b019d2159428d3c55c
jjlee050 <josephlee050@gmail.com>
2019-01-20 09:53:41 +0800

    Add config file.

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+define('DEVELOPMENT',true);
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cryptofaucet');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+ 
+define('BASEPATH', 'http://localhost:8888/freecryptoplz/');
+define('ASSETPATH', 'http://localhost:8888/freecryptoplz/assets/');
+define('MODELPATH','model/');
+define('CONTROLLERPATH','controller/');
+define('VIEWPATH','view/');
+define('LIBPATH','libs/');
+ 
+define('DEFAULTCONTROLLER', 'main');
+?>
