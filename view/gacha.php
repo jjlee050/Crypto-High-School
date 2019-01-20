7e3c452475c8b65fd3e13939df7ce61924d1afcf
jjlee050 <josephlee050@gmail.com>
2019-01-20 09:32:37 +0800

    Add carousel sliding window to gacha.

--- view/gacha.php
+++ view/gacha.php
@@ -1,4 +1,33 @@
 <div id="gacha">
+
+<!-- NEW CAROUSEL -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100 h-25" src="<?php echo ASSETPATH; ?>img/samples/1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100 h-25" src="<?php echo ASSETPATH; ?>img/samples/2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100 h-25" src="<?php echo ASSETPATH; ?>img/samples/3.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
     <form action="gacha/collectCard" method="post">
         <div class="card">
             <div class="card-header">
